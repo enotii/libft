cdffc3d62a073cd2b603cb0047bc560b17272c21
Кристина Зуева <kristinazueva@MacBook-Pro-Kris.local>
2018-12-11 19:10:31 +0300

    strnstr.c

--- srcs/header.h
+++ srcs/header.h
@@ -31,4 +31,5 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 char    *ft_strstr(const char *haystack, const char *needle);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 #endif
